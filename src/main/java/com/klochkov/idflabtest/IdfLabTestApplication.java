d641385dd56279cfb6b335b64deb8cd32985fa6b
vladimir <klochkov.777@mail.ru>
2024-04-03 23:38:01 +0300

    init dev
    remove comment pom.xml

---
+++ src/main/java/com/klochkov/idflabtest/IdfLabTestApplication.java
@@ -0,0 +1,15 @@
+package com.klochkov.idflabtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdfLabTestApplication {
+	/**
+	 * @param args - args
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(IdfLabTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/klochkov/idflabtest/IdfLabTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klochkov.idflabtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdfLabTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
